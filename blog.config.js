2f7ed821b8270d537c2bd74a37906f81d1e0a4ab
icelo <icelo@icelo-mbp.local>
2024-11-07 22:11:06 +0800

    修改域名相关

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: '冰洛博客',
   author: 'icelo',
   email: 'i@icelo.cn',
-  link: 'https://icelo.cn',
+  link: 'https://icelo.org',
   description: 'Coding On My Life',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
